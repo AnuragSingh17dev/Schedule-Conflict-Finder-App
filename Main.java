e9cce813fd21be35f31440cd29d8db90d5d2169f
Anurag Singh <anurag.dhanju@gmail.com>
2024-08-01 15:42:36 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,9 +3,8 @@ import javax.swing.*;
 public class Main {
 public static void main(String[] args) {
     SwingUtilities.invokeLater(new Runnable() {
-        @Override
         public void run() {
-            ScheduleConflictFinder gui = new ScheduleConflictFinder();
+        	ScheduleConflictFinder gui = new ScheduleConflictFinder();
             gui.setVisible(true);
         }
     });
